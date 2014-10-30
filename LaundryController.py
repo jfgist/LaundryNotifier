8e1a2cc903d65b63efe3c6c997e9742b9c761064
jfgist <james.gist@gmail.com>
2014-10-30 01:17:45 +0000

    Updating some stuff

--- LaundryController.py
+++ LaundryController.py
@@ -4,15 +4,17 @@ import socket
 from email.mime.text import MIMEText
 import RPi.GPIO as GPIO
 import datetime
+import time
 
 #GPIO Setup
+print("GPIO Setup")
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17,GPIO.IN)
 
 # Change to your own account information
 to = 'james.gist@gmail.com'
 gmail_user = 'raspberrypi1409@gmail.com'
-gmail_password = 'boynumber'
+gmail_password = ''
 smtpserver = smtplib.SMTP('smtp.gmail.com', 587)
 smtpserver.ehlo()
 smtpserver.starttls()
@@ -26,19 +28,23 @@ prev_input = 0;
 while True:
   #take a reading
   input = GPIO.input(17)
+  print("Input: ")
+  print(input)
+  time.sleep(0.1)
   #if reading went from low to high
   if((not prev_input) and input):
     print("Button Pressed")
+    prev_input = input
+    msg = MIMEText('Content')
     msg['Subject'] = 'Message from RaspberryPi on %s' % today.strftime('%b %d %Y')
     msg['From'] = gmail_user
     msg['To'] = to
     smtpserver.sendmail(gmail_user, [to], msg.as_string())
     smtpserver.quit()
 
-    #update previous input
-    prev_input = input
     #slight pause to debounce
-    time.sleep(0.1)
+    time.sleep(0.5)
+    prev_input = 0
     
 
 
