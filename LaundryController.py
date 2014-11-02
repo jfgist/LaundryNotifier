8dfdd07a33374bc92481474eba459fd74dfed3a2
jfgist <James.Gist@gmail.com>
2014-11-01 21:04:15 -0600

    Update LaundryController.py

--- LaundryController.py
+++ LaundryController.py
@@ -17,33 +17,35 @@ gmail_user = 'raspberrypi1409@gmail.com'
 gmail_password = ''
 today = datetime.date.today()
 
-prev_input = 0;
+buttonPressed = false
+
 while True:
   #take a reading
   input = GPIO.input(17)
-  print("Input: ")
-  print(input)
+  print("Input: " + input)
   time.sleep(0.1)
-  #if reading went from low to high
-  if((not prev_input) and input):
-    print("Button Pressed")
-    time.sleep(3600)
-    prev_input = input
-    msg = MIMEText('ROTATE YO LAUNDRY')
-    msg['Subject'] = 'Pi-Notify on %s' % today.strftime('%b %d %Y')
-    msg['From'] = gmail_user
-    msg['To'] = to
-    smtpserver = smtplib.SMTP('smtp.gmail.com', 587)
-    smtpserver.ehlo()
-    smtpserver.starttls()
-    smtpserver.ehlo
-    smtpserver.login(gmail_user, gmail_password)
-    smtpserver.sendmail(gmail_user, [to], msg.as_string())
-    smtpserver.close()
+  
+  if(input & not buttonPressed):
+    print('Button Pressed')
+    buttonPressed = true
+    buttonPressTime = time.time()
+    time.sleep(1)
 
-    #slight pause to debounce
-    time.sleep(0.5)
-    prev_input = 0
-    
+  if (buttonPressed):
+    if ((buttonPressTime - time.time()) > 3600):
+      msg = MIMEText('ROTATE YO LAUNDRY')
+      msg['Subject'] = 'Pi-Notify on %s' % today.strftime('%b %d %Y')
+      msg['From'] = gmail_user
+      msg['To'] = to
+      smtpserver = smtplib.SMTP('smtp.gmail.com', 587)
+      smtpserver.ehlo()
+      smtpserver.starttls()
+      smtpserver.ehlo
+      smtpserver.login(gmail_user, gmail_password)
+      smtpserver.sendmail(gmail_user, [to], msg.as_string())
+      smtpserver.close()
+      
+      buttonPressed = false
+
 
 
