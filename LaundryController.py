d80ac8f681d0b9071447bef2fc3cc64fbf56209e
jfgist <james.gist@gmail.com>
2015-01-06 18:55:05 +0000

    Controller updated with delay for capactiive button and sensitive data moved to config file

--- LaundryController.py
+++ LaundryController.py
@@ -5,40 +5,62 @@ from email.mime.text import MIMEText
 import RPi.GPIO as GPIO
 import datetime
 import time
+import sys
 
 #GPIO Setup
 print("GPIO Setup")
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18,GPIO.IN)
 
-# Change to your own account information
-to = 'james.gist@gmail.com'
-gmail_user = 'raspberrypi1409@gmail.com'
-gmail_password = ''
-today = datetime.date.today()
+# Setup email timer delay
+delay = 3 # default to one hour
+print delay
 
+if len(sys.argv) > 1:
+  print delay
+  print('here')
+  delay = int(sys.argv[1])
+
+print(delay)
+
+# read the to email, from email and from password from config file
+emailConfigFile ='/home/pi/tools/email.conf'
+params = [line.strip() for line in open(emailConfigFile)]
+
+if len(params) >= 3:
+  to = params[0]
+  gmail_user = params[1]
+  gmail_password = params[2]
+
+print(to)
+print(gmail_user)
+print(gmail_password)
+
+today = datetime.date.today()
 buttonPressed = False
 
 while True:
   #take a reading
   input = GPIO.input(18)
-  print("Input: ")
-  print(input)
-  print("IsButton Pressed:")
-  print(buttonPressed)
   time.sleep(0.1)
   
   if(input and not(buttonPressed)):
-    print('Button Pressed')
-    buttonPressed = True
-    buttonPressTime = time.time()
+    print('Button Pressed First')
+    time.sleep(3)
+    input = GPIO.input(18)
+    if (input):
+      print("Button held for X seconds")
+      buttonPressed = True
+      buttonPressTime = time.time()
     time.sleep(1)
 
   if (buttonPressed):
-    time.sleep(60)
-    if ((time.time() - buttonPressTime) > 3600):
+    print("Button Pressed True")
+    print((time.time()) - buttonPressTime)
+    print(delay)
+    if ((time.time() - buttonPressTime) > delay):
       print("Send Email")
-      msg = MIMEText('ROTATE YO LAUNDRY')
+      msg = MIMEText('ROTATE YOUR LAUNDRY')
       msg['Subject'] = 'Pi-Notify on %s' % today.strftime('%b %d %Y')
       msg['From'] = gmail_user
       msg['To'] = to
